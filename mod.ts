6e26885468548f3de1bbe700172f10a7a1d3e075
Luca Casonato <hello@lcas.dev>
2022-02-21 16:42:21 +0100

    break ci

--- mod.ts
+++ mod.ts
@@ -3,7 +3,7 @@ export class JSONParseStream extends TransformStream<string, unknown> {
     super({
       transform(chunk, controller) {
         const obj = JSON.parse(chunk);
-        controller.enqueue(obj);
+        controller.enqueue(obj)
       },
     });
   }
